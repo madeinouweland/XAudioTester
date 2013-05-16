13a7b762aec4ad55eba0af947a85ec6fdf1a252d
madeinouweland <Loek@Mackenstein.local>
2013-05-16 12:40:40 +0200

    Cleanup

---
+++ XAudioTester/XAudioTester/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace XAudioTester {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+        }
+
+        private void Button_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e) {
+            XPlayer.Instance.PlayWave(0);
+        }
+
+        private void Button_Tapped_1(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e) {
+            XPlayer.Instance.PlayWave(1);
+        }
+    }
+}
---
+++ XAudioTester/XAudioTester/XPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using XAudioPlayer;
+
+namespace XAudioTester {
+    public class XPlayer : IDisposable {
+        private XAudio2SoundPlayer player;
+
+        private static XPlayer instance;
+        public static XPlayer Instance {
+            get {
+                if (instance == null) {
+                    instance = new XPlayer();
+                }
+                return instance;
+            }
+        }
+
+        public XPlayer() {
+            player = new XAudio2SoundPlayer();
+            var audioCount = player.LoadSounds();
+
+        }
+
+        public void PlayWave(uint index) {
+            player.PlaySound(index);
+        }
+
+        public void Dispose() {
+        }
+    }
+}
